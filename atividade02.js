d160cd2af6d368cf086c0212df771352cf080922
MarceloSilva05 <jmarcelosilva.2021@gmail.com>
2021-09-27 17:50:22 -0300

    primeiro commit

---
+++ atividade02.js
@@ -0,0 +1,14 @@
+
+//1
+let tabuada=[5,10,15,20,25,30,35,40,45,50];
+tabuada=tabuada.join();
+console.log(tabuada);
+
+//2 (n-32)/1.8
+let n=[54, 78, 56, 98, 0, 12, 11, 37];
+let total;
+n.forEach(function(item){
+console.log((item-32)/1.8)
+})
+
+
